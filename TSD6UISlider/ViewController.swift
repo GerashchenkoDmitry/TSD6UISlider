f1661df8cdb71038d72ec1d5a8147916fce0f3db
GerashchenkoDmitry <finiqw@gmail.com>
2022-02-10 14:13:30 +0700

    Initial Commit

---
+++ TSD6UISlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TSD6UISlider
+//
+//  Created by Дмитрий Геращенко on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
